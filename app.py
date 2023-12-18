bec2025864a8d45e6b8e505715314e27d1a290f7
José Francisco Azevedo da Silva <jose@portoaz.com.br>
2023-12-17 22:28:58 -0300

    funções

--- app.py
+++ app.py
@@ -1,33 +1,54 @@
+import os
+
 # apresentação 
 
-print("""
-      
-░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
-██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
-╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
-░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
-██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
-      """)
+def apresentacao():
+    print("""
+          
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+    """)
 
-# Menu
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
+def exibir_menu():
 
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
 
-# inicio da aplicação
-opcao_escolhida = int(input('Escolha uma opção: '))
-
-if opcao_escolhida == 1:
-    print('Cadastrar restaurante')
-
-if opcao_escolhida == 2:
-    print('Listar restaurante')
-
-if opcao_escolhida == 3:
-    print('Ativar restaurante')
-
-if opcao_escolhida == 4:
+def finalizar_app():
+    os.system('cls')
     print('Encerrando o  Processo')
+
+
+def escolher_opcoes():
+
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+
+    if opcao_escolhida == 2:
+        print('Listar restaurante')
+
+    if opcao_escolhida == 3:
+        print('Ativar restaurante')
+
+    if opcao_escolhida == 4:
+        finalizar_app()
+
+def main():
+
+    apresentacao()
+
+    exibir_menu()
+
+    escolher_opcoes()
+
+if __name__ == '__main__':
+    main()
