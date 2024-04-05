cb5e5a22dbc84cdf6fba8b6ca1fe528f5b5dac71
José Francisco Azevedo da Silva <jose@portoaz.com.br>
2024-04-05 20:47:49 -0300

    Exercicio3 inicio do 4

---
+++ Exercicio3.py
@@ -0,0 +1,52 @@
+# 3 - Solicite um nome de usuário e uma senha e use uma estrutura if else para verificar se o nome de usuário e a senha fornecidos correspondem aos valores esperados determinados por você.
+
+import os
+
+def limpa_tela():
+    
+    os.system('cls')
+
+usuario = 'joseazev'
+senha   = '2457'
+
+def apresentacao():
+    print("""
+
+░█████╗░░█████╗░███████╗░██████╗░██████╗░█████╗░  ░█████╗░░█████╗░
+██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝██╔══██╗  ██╔══██╗██╔══██╗
+███████║██║░░╚═╝█████╗░░╚█████╗░╚█████╗░██║░░██║  ███████║██║░░██║
+██╔══██║██║░░██╗██╔══╝░░░╚═══██╗░╚═══██╗██║░░██║  ██╔══██║██║░░██║
+██║░░██║╚█████╔╝███████╗██████╔╝██████╔╝╚█████╔╝  ██║░░██║╚█████╔╝
+╚═╝░░╚═╝░╚════╝░╚══════╝╚═════╝░╚═════╝░░╚════╝░  ╚═╝░░╚═╝░╚════╝░
+
+░██████╗██╗░██████╗████████╗███████╗███╗░░░███╗░█████╗░
+██╔════╝██║██╔════╝╚══██╔══╝██╔════╝████╗░████║██╔══██╗
+╚█████╗░██║╚█████╗░░░░██║░░░█████╗░░██╔████╔██║███████║
+░╚═══██╗██║░╚═══██╗░░░██║░░░██╔══╝░░██║╚██╔╝██║██╔══██║
+██████╔╝██║██████╔╝░░░██║░░░███████╗██║░╚═╝░██║██║░░██║
+╚═════╝░╚═╝╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░░░░╚═╝╚═╝░░╚═╝
+""")
+
+def verrificar_identidade(usuario,usuario_escrito,senha,senha_escrita):
+    if usuario_escrito == usuario:
+        if senha == senha_escrita:
+            print(f'Bem vindo {usuario}')
+        else:
+            print(f'senha incorreta')
+    else:
+        print('Usuário não encontrado')
+
+
+def main():
+    
+    limpa_tela()
+    apresentacao()
+
+    usuario_escrito = input('Informe o usuário: ')
+    senha_escrita   = input('Informe a senha:   ')
+
+    verrificar_identidade(usuario,usuario_escrito,senha,senha_escrita)
+
+if __name__ == '__main__':
+    main()
+
