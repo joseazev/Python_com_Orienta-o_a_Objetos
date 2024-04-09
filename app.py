2e6e28b388767286a4915cb6f2077fd07709b77f
José Francisco Azevedo da Silva <jose@portoaz.com.br>
2024-04-09 19:22:03 -0300

    Organizanfo os arquivos

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os
 
-def finalizar_app():
+restaurantes = ['Caipira','Três Marias','Xian Rio']
+
+def limpar_tela(subtitulo):
     os.system('cls')
+    print(subtitulo)
+
+
+def finalizar_app():
+    limpar_tela()
     print('Finalizando App\n')
 
 def exibir_nome_app():
@@ -22,28 +29,59 @@ def exibir_opcoes():
     print(f'3. Ativar Restaurante')
     print(f'4. Sair\n')
 
+def opcao_invalida():
+    print('opção inválida\n')
+    voltar_ao_menu_principal()
+
+def voltar_ao_menu_principal():
+    input(f'\nTecle ENTER para voltar ao menu principal')
+    main()
+
+def cadastrar_novo_restaurante():
+    limpar_tela('Cadastrar Restaurantes')
+    novo_retaurante = input('informe nome do restaurante que deseja cadastrar:\n')
+    restaurantes.append(novo_retaurante)
+    limpar_tela()
+    print(f'\nRestaurante {restaurantes[-1]} foi cadastrado com sucesso')
+    voltar_ao_menu_principal()
+
+def listar_retaurantes():
+    limpar_tela('Listando Restaurantes\n')
+
+    for restaurante in restaurantes:
+        print(f'.{ restaurante}')
+
+    voltar_ao_menu_principal()
+
 def escolher_opcoes():
     opcao_escolhida = int(input(f'Escolha uma opção '))
     print(f'Você escolheu a opção {opcao_escolhida}!')
 
-    match opcao_escolhida:
-        case 1:
-            print('Cadastrar Restaurantes')
+    try:
+        match opcao_escolhida:
+            case 1:
+                
+                cadastrar_novo_restaurante()
 
-        case 2:
-            print('Listar Restaurantes')
+            case 2:
+                listar_retaurantes()
 
-        case 3:
-            print('Ativar Restaurantes')
+            case 3:
+                print('Ativar Restaurantes')
 
-        case 4:
-            finalizar_app()
-        
-        case _:
-            print('opção inválida')
+            case 4:
+                finalizar_app()
+            
+            case _:
+                opcao_invalida()
+    
+    except:
+        opcao_invalida()
 
 def main():
-    exibir_nome_app()
+    limpar_tela()
+
+    exibir_nome_app()   
 
     exibir_opcoes()
 
